5d97ab8a60c50ac2097b528b48e9d1c244d23aa6
farruxxamidov <farruxxamidov@gmail.com>
2020-03-12 11:33:02 +0500

    feat: resolve dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,25 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  // for (let i=0; i<members.length; i++) {
+  //   if (typeof(members[i]) !== 'string') {
+  //     members.splice(i, 1);
+  //   }
+  // }
+  // members.sort();
+  // for (let i=0; i<members.length; i++) {
+  //   teamName.concat(members[i].charAt[0]);
+  // }
+  let teamName='';
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  for (let i=0; i<members.length; i++) {
+    if (typeof(members[i]) === 'string') {
+      //teamName.concat(members[i].charAt(0));
+      let withoutSpace = members[i].trim();
+      teamName += withoutSpace.charAt(0).toUpperCase();
+    } else {
+      continue;
+    }
+  }
+  return teamName.split('').sort().join('');
+}
