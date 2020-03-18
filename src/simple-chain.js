b57045fe69326e36b3b75043eefd5244cea55d71
farruxxamidov <farruxxamidov@gmail.com>
2020-03-18 06:48:03 +0500

    feat: resolve 'simple-chain' task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,42 @@
+let chainArr = [];
 const chainMaker = {
+  //chain: [],
+  //chainOutput: '',
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return chainArr.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    //console.log(value);
+    chainArr.push(value);
+    //console.log(chainArr);
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (typeof position !== 'number' || position % 1 !== 0) {
+      chainArr.length = 0;
+      throw new Error;
+    } else if (position <= 0 || position > chainArr.length) {
+      chainArr.length = 0;
+      throw new Error;
+    }
+    chainArr.splice(position - 1, 1);
+    //console.log(chainArr);
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    chainArr.reverse();
+    //console.log(chainArr);
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let chainOutput = '';
+    for (let i = 0; i < chainArr.length - 1; i++) {
+      chainOutput += '( ' + chainArr[i] + ' )' + '~~';
+    }
+    chainOutput += '( ' + chainArr[chainArr.length - 1] + ' )';
+    //console.log(chainOutput);
+    chainArr.length = 0;
+    return chainOutput;
   }
 };
 
