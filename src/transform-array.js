59d4972d1ef1423d85eeae1a12f6afdb3a1248a4
farruxxamidov <farruxxamidov@gmail.com>
2020-03-20 10:28:24 +0500

    feat: resolve task 'transform-array.js'

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,39 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+    if (!Array.isArray(arr)) throw new Error();
+
+    let transformArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case '--discard-next':
+            case '--discard-prev':
+            case '--double-next':
+            case '--double-prev':
+                continue;
+        }
+
+        transformArr.push(arr[i]);
+
+        switch (arr[i + 1]) {
+            case '--double-prev':
+                transformArr.push(arr[i]);
+                break;
+            case '--discard-prev':
+                transformArr.splice(transformArr.length - 1, 1);
+                break;
+        }
+
+        switch (arr[i - 1]) {
+            case '--double-next':
+                transformArr.push(arr[i]);
+                break;
+            case '--discard-next':
+                transformArr.splice(transformArr.length - 1, 1);
+                break;
+        }
+    }
+
+    return transformArr;
+
 };
